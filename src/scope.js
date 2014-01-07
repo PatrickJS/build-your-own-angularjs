0607b834c6fa6ef748b237c963db778c0350b58f
gdi2290 <github@gdi2290.com>
2014-01-06 20:20:18 -0800

    add $watch to Scope prototype

--- src/scope.js
+++ src/scope.js
@@ -1,3 +1,12 @@
 function Scope() {
+  // $$ are private and should not be called from app code
   this.$$watchers = [];
 }
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watch: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
