b711023d9b4e40a83b4db88abbc3692b10fbc36d
gdi2290 <github@gdi2290.com>
2014-01-06 18:45:16 -0800

    add jshint config in and load task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,16 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
+    jshint: {
+      all: ['src/**/*.js'],
+      options: {
+        globals: {
+          _: false,
+          $: false
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 };
